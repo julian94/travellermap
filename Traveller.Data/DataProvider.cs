a03e0e8d80c8d27e95b5ce1adb4a41c0351ff6e9
Julian Silden Langlo <langlo94@gmail.com>
2021-11-14 13:39:39 +0100

    Update DataProvider.

---
+++ Traveller.Data/DataProvider.cs
@@ -0,0 +1,23 @@
+﻿using Traveller.Core.Features;
+using Traveller.Parser;
+
+namespace Traveller.Resources;
+
+public class DataProvider
+{
+    public DataProvider()
+    {
+        /* Todo:
+         * 1: Read in and parse all Sectors and milieus.
+         *      a: Enumerate folders and files into a tree structure.
+         *      b: Fetch the Main xml metadata file, names match folders.
+         *      c: Parse sectors, one by one.
+         * 2: Present the data in an easy to use data structure.
+         * 3: Add test mileu and tests to verify proper functioning.
+         */
+
+        Galaxies = new();
+    }
+
+    public List<Galaxy> Galaxies { get; init; }
+}
